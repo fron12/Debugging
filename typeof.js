05b397482a5cca14faea1c02bc2fe51bb003d344
Fred <sohnf90@yahoo.com>
2019-03-01 14:32:04 -0800

    Using typeof

---
+++ typeof.js
@@ -0,0 +1,14 @@
+console.log(typeof "");
+console.log(typeof 1);
+console.log(typeof []);
+console.log(typeof {});
+
+let one = 1;
+let two = "2";
+let sum = one + two;
+console.log(one);
+console.log(two);
+console.log(sum);
+console.log(typeof one);
+console.log(typeof two);
+console.log(typeof sum);
